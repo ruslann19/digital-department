4ec8aed54c9cef2824cb502aec2e8d2a2b7545a9
ruslan <runovikov2@gmail.com>
2024-05-02 02:37:43 +0300

    added admin-panel

--- web-developer/3-web/project_tracker/quality_control/admin.py
+++ web-developer/3-web/project_tracker/quality_control/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from .models import BugReport, FeatureRequest
 
-# Register your models here.
+@admin.action(description='Поменять статус на "Завершена"')
+def change_status_to_Completed(self, request, queryset):
+    queryset.update(status='Completed')
+    self.message_user(request, f"Статус изменён на 'Завершено' у {queryset.count()} записи(ей).")
+
+@admin.action(description='Поменять статус на "Новая"')
+def change_status_to_New(self, request, queryset):
+    queryset.update(status='New')
+    self.message_user(request, f"Статус изменён на 'Новая' у {queryset.count()} записи(ей).")
+
+@admin.action(description='Поменять статус на "В работе"')
+def change_status_to_In_progress(self, request, queryset):
+    queryset.update(status='In_progress')
+    self.message_user(request, f"Статус изменён на 'В работе' у {queryset.count()} записи(ей).")
+
+@admin.register(BugReport)
+class BugReportAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'task', 'created_at')
+    list_filter = ('status', 'project')
+    search_fields = ('title', 'description')
+    actions = [change_status_to_Completed, change_status_to_New, change_status_to_In_progress]
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'description', 'created_at',)
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': ('project', 'task', 'status', 'priority',),
+        }),
+    )
+    readonly_fields = ('created_at', 'updated_at',)
+
+
+@admin.register(FeatureRequest)
+class FeatureRequestAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'task', 'created_at')
+    list_filter = ('status', 'project')
+    search_fields = ('title', 'description')
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'description', 'created_at',)
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': ('project', 'task', 'status', 'priority',),
+        }),
+    )
+    readonly_fields = ('created_at', 'updated_at',)
--- web-developer/3-web/project_tracker/tasks/models.py
+++ web-developer/3-web/project_tracker/tasks/models.py
@@ -39,3 +39,6 @@ class Task(models.Model):
         choices=STATUS_CHOICES,
         default='New',
     )
+
+    def __str__(self):
+        return self.name
