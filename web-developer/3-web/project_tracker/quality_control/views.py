bbb984a26aac0fea27d0ade2004235f4beb15dc7
ruslan <runovikov2@gmail.com>
2024-05-03 18:22:27 +0300

    configured url

--- web-developer/3-web/project_tracker/quality_control/views.py
+++ web-developer/3-web/project_tracker/quality_control/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import reverse
 
-# Create your views here.
+def index(request):
+    bugs_url = reverse('quality_control:bug_list')
+    features_url = reverse('quality_control:feature_list')
+    html = f'''
+        <h1>Система контроля качества</h1>
+        <a href='{bugs_url}'>Список всех багов</a><br>
+        <a href='{features_url}'>Запросы на улучшение</a></br>
+    '''
+    return HttpResponse(html)
+
+def bug_list(request):
+    bugs_html = '<h1>Cписок отчетов об ошибках</h1>'
+    return HttpResponse(bugs_html)
+
+def feature_list(request):
+    features_html = '<h1>Список запросов на улучшение</h1>'
+    return HttpResponse(features_html)
+
+def bug_detail(request, bug_id):
+    response_html = f'<h1>Детали бага {bug_id}</h1>'
+    return HttpResponse(response_html)
+
+def feature_detail(request, feature_id):
+    response_html = f'<h1>Детали улучшения {feature_id}</h1>'
+    return HttpResponse(response_html)
